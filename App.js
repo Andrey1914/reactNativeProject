d47476436ce7ad0e4d13620eae17930fbae536b1
Andrey1914 <anlikajw@gmail.com>
2022-12-04 19:52:26 +0100

    need to fix

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text } from "react-native";
+// import {} from "react-native";
+// import { AppLoading } from "expo";
+
+// import RegisterScreen from "./screens/RegistrationScreen";
+// import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
+  // const [isReady, setIsReady] = useState(false);
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading onFinish={() => setIsReady(true)} onError={console.warn} />
+  //   );
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is my first React Native App!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text>Test</Text>
+      {/* <RegisterScreen />
+      <LoginScreen /> */}
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fcfcfc",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <Text>This is my first React Native App!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fcfcfc",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from "react-native";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export default function LoginScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get("window").width - 20 * 2
+  );
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 20 * 2;
+      setDimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <View
+            style={{
+              ...styles.form,
+              marginBottom: isShowKeyboard ? 20 : 150,
+              width: dimensions,
+            }}
+          >
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Welcome back!</Text>
+            </View>
+            <View>
+              <Text style={styles.inputTitle}>Email addres</Text>
+              <TextInput
+                style={styles.input}
+                name="hello"
+                textAlign={"center"}
+                onFocus={() => setIsShowKeyboard(true)}
+                value={state.email}
+                onChange={(nativeEvent) => console.log(nativeEvent)}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, email: value }))
+                }
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>Password</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                onFocus={() => setIsShowKeyboard(true)}
+                value={state.password}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, password: value }))
+                }
+              />
+            </View>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              style={styles.btn}
+              onPress={keyboardHide}
+            >
+              <Text style={styles.btnTitle}>Sign in</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "f0f8ff",
+    height: 40,
+    borderRadius: 8,
+    color: "#f0f8ff",
+  },
+  inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+  },
+  btn: {
+    borderRadius: 6,
+    borderWidth: 1,
+    height: 40,
+    marginTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "transparent",
+        borderColor: "#f0f8ff",
+      },
+      android: {
+        backgroundColor: "#4169e1",
+        borderColor: "transparent",
+      },
+    }),
+  },
+  btnTitle: {
+    color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 120,
+  },
+  headerTitle: {
+    color: "#f0f8ff",
+  },
+});

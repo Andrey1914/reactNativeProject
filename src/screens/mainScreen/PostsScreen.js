092f3ecf07b43575570b111746cdf76bcafc8ce0
Andrey1914 <anlikajw@gmail.com>
2022-12-27 11:05:28 +0100

    Posts Screen

--- src/screens/mainScreen/PostsScreen.js
+++ src/screens/mainScreen/PostsScreen.js
@@ -1,25 +1,226 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-const PostsScreen = () => {
+import {
+  collection,
+  getDocs,
+  getDoc,
+  arrayUnion,
+  arrayRemove,
+  doc,
+  updateDoc,
+  onSnapshot,
+} from "firebase/firestore";
+
+import { Ionicons } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function PostsScreen({ navigation, route }) {
+  const [posts, setPosts] = useState([]);
+  const { email, login, avatar, userId } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  const getAllPosts = async () => {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    let newPosts = [];
+    querySnapshot.forEach((doc) => {
+      newPosts.push({ ...doc.data(), id: doc.id });
+    });
+    setPosts(newPosts);
+  };
+
+  const addLike = async (id) => {
+    const result = await getDoc(doc(db, "posts", `${id}`));
+    if (result.data().likes.includes(`${userId}`)) {
+      await updateDoc(doc(db, "posts", `${id}`), {
+        likes: arrayRemove(`${userId}`),
+      });
+    } else {
+      await updateDoc(doc(db, "posts", `${id}`), {
+        likes: arrayUnion(`${userId}`),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts"),
+      (snapshot) => {
+        getAllPosts();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Posts Screen</Text>
+      <View
+        style={{
+          marginHorizontal: 16,
+          marginTop: 32,
+          marginBottom: 70,
+        }}
+      >
+        <TouchableOpacity
+          style={styles.userBox}
+          onPress={() => navigation.navigate("Profile")}
+        >
+          {avatar ? (
+            <Image
+              source={{ uri: avatar }}
+              style={{
+                height: 60,
+                width: 60,
+                borderRadius: 16,
+              }}
+            ></Image>
+          ) : (
+            <View
+              style={{
+                width: 60,
+                width: 60,
+                borderRadius: 16,
+                backgroundColor: "#515151",
+              }}
+            ></View>
+          )}
+          <View style={{ marginLeft: 8 }}>
+            <Text style={styles.textName}>{login}</Text>
+
+            <Text style={styles.textEmail}>{email}</Text>
+          </View>
+        </TouchableOpacity>
+
+        <FlatList
+          data={posts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.postBox}>
+              <Image
+                source={{ uri: item.photo }}
+                style={{ height: 240, borderRadius: 16 }}
+              ></Image>
+
+              <View style={{ marginTop: 8 }}>
+                <Text style={styles.textPost}>{item.name}</Text>
+              </View>
+
+              <View style={styles.postInfoBox}>
+                <View style={styles.commentsInfo}>
+                  <TouchableOpacity
+                    onPress={async () => {
+                      navigation.navigate("Comments", {
+                        photo: item.photo,
+                        id: item.id,
+                      });
+                      dispatch(pathSlice.actions.setPath({ path: route.name }));
+                    }}
+                  >
+                    <EvilIcons name="comment" size={24} color="#fff" />
+                  </TouchableOpacity>
+
+                  <Text style={styles.textPost}>{item.comments || 0}</Text>
+                </View>
+
+                <View style={styles.commentsInfo}>
+                  <TouchableOpacity onPress={() => addLike(item.id)}>
+                    {item.likes.includes(`${userId}`) ? (
+                      <AntDesign name="like1" size={24} color="#fff" />
+                    ) : (
+                      <AntDesign name="like2" size={24} color="#fff" />
+                    )}
+                  </TouchableOpacity>
+
+                  <Text style={styles.textPost}>
+                    {" "}
+                    {item.likes?.length || 0}
+                  </Text>
+                </View>
+
+                <View style={styles.locationInfo}>
+                  <Ionicons name="location-outline" size={20} color="#fff" />
+
+                  <Text
+                    style={styles.textLocation}
+                    onPress={() => {
+                      navigation.navigate("Map", {
+                        location: item.location,
+                        title: item.locationName,
+                      }),
+                        dispatch(
+                          pathSlice.actions.setPath({ path: route.name })
+                        );
+                    }}
+                  >
+                    {item.locationName}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          )}
+        ></FlatList>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    backgroundColor: "#121212",
   },
-  title: {
-    fontFamily: "Montserrat-Thin",
-    fontSize: 32,
+  userBox: {
+    marginBottom: 32,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textName: {
+    color: "#FFF",
+    fontFamily: "DMMono-Medium",
+    fontSize: 13,
+  },
+  textEmail: {
+    color: "#515151",
+    fontFamily: "DMMono-Regular",
+    fontSize: 11,
+  },
+  textPost: {
+    color: "#FFF",
+    fontFamily: "DMMono-Medium",
+    fontSize: 16,
+  },
+  textLocation: {
+    color: "#FFF",
+    fontFamily: "DMMono-Regular",
+    fontSize: 16,
+    textDecorationLine: "underline",
+  },
+  postBox: {
+    marginBottom: 34,
+  },
+  postInfoBox: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 11,
+  },
+  commentsInfo: {
+    flexDirection: "row",
+  },
+  locationInfo: {
+    flexDirection: "row",
   },
 });
-
-export default PostsScreen;

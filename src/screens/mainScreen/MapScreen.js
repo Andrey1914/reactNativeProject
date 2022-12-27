0417745a200070e2d7e624a03baabd24e10064e7
Andrey1914 <anlikajw@gmail.com>
2022-12-27 10:01:29 +0100

    Map Screen

--- src/screens/mainScreen/MapScreen.js
+++ src/screens/mainScreen/MapScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const MapScreen = () => (
-  <View style={styles.container}>
-    <Text>MapScreen</Text>
-  </View>
-);
+export default function MapScreen({ route }) {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={{ flex: 1, marginHorizontal: 16 }}
+        initialRegion={{
+          longitude: route.params.location.coords.longitude,
+          latitude: route.params.location.coords.latitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.006,
+        }}
+      >
+        <Marker
+          coordinate={{
+            longitude: route.params.location.coords.longitude,
+            latitude: route.params.location.coords.latitude,
+          }}
+          title={route.params.title}
+        ></Marker>
+      </MapView>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#121212",
+  },
+  text: {
+    textAlign: "center",
+    color: "#FFF",
   },
 });
-
-export default MapScreen;
